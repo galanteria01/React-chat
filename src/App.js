193065c72069aff87d34add3f49e284cf050cf13
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-03-19 20:10:47 +0530

    Added engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./styles.css";
 import Login from "./files/Login";
+import { ChatEngine } from 'react-chat-engine'
+
 
 export default function App() {
   return (
     <div className="App">
-      <Login />
+      <ChatEngine
+			height='100vh'
+			userName='Shoury'
+			userSecret='baatcheet'
+			projectID='30413a6f-5cf7-4635-ab12-748617ce371c'
+		/>
     </div>
   );
 }
